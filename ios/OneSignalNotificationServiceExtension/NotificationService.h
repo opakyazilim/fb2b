ed846189eeb3301789550a7244b46022d73a44d0
muratbaz <muratbaz@gmail.com>
2023-09-28 01:50:19 -0700

    son

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by opakMurat on 26.09.2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
---
+++ ios/OneSignalNotificationServiceExtension2/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension2
+//
+//  Created by opakMurat on 26.09.2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
