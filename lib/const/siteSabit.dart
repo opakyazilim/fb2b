c4186893d36110f6a7e4ee001abdf7906de47366
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2023-10-24 17:09:08 +0300

    Gönderen Kullanıcı :Fatih Uygulama : PONICIOTOYS

--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -2,7 +2,7 @@ class SiteSabit {
    static String? FirmaAdi = "PONICIOTOYS";  
   static String? Link = "b2b.poniciotoys.com"; 
   static String? Com = "com.opakyazilim.poniciotoysb2b"; 
-  static String? Versiyon = "0.0.4"; 
+  static String? Versiyon = "0.0.5"; 
   static String? Platform = "IOS"; 
   static String? ExServisId = "C808B7DD-2F6C-40CC-A128-5B964C008FA3"; 
   static String oneSignalKey = "5db19a4e-48e3-4230-b089-f925c3f11c06"; 
