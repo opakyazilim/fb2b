64952672c6656714e5ccef6c9317b3f0837adcbb
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2023-10-25 11:50:13 +0300

    Gönderen Kullanıcı :Fatih Uygulama : HYTKIRTASIYE

--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -2,7 +2,7 @@ class SiteSabit {
    static String? FirmaAdi = "HYTKIRTASIYE";  
   static String? Link = "hytkirtasiye.com.tr"; 
   static String? Com = "com.opakyazilim.b2bhytkirtasiye"; 
-  static String? Versiyon = "0.0.6"; 
+  static String? Versiyon = "0.0.7"; 
   static String? Platform = "IOS"; 
   static String? ExServisId = "9F35BCDC-365D-4CD5-B52F-1AC9B0C1692C"; 
   static String oneSignalKey = "b5b6b085-b9b3-45b7-bb44-bdf4e7cbe9bd"; 
