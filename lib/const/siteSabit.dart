dc4548eeaa9a80977d8719b3d4fbd02bdfcbb97a
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2023-10-28 15:24:38 +0300

    Gönderen Kullanıcı :Fatih Uygulama : FUNBINO

--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -4,6 +4,6 @@ class SiteSabit {
   static String? Com = "com.opakyazilim.funbinob2b"; 
   static String? Versiyon = "0.0.1"; 
   static String? Platform = "IOS"; 
-  static String? ExServisId = "A19CDA04-1853-4341-AC93-FB42EF9A8C6B"; 
+  static String? ExServisId = "A9BD4C51-EA9B-4AFA-9A41-3DC09904AB58"; 
   static String oneSignalKey = "010d6f41-d9e6-4ca9-b717-1d57b988da04"; 
 }
