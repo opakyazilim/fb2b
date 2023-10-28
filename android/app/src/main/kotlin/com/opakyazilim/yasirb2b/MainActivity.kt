f5d1d008fde18c987cb7b3156c7bde87833d8c1f
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2023-10-28 13:23:41 +0300

    Gönderen Kullanıcı :Fatih Uygulama : YASIR

--- android/app/src/main/kotlin/com/opakyazilim/yasirb2b/MainActivity.kt
+++ android/app/src/main/kotlin/com/opakyazilim/yasirb2b/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.opakyazilim.emirkirtasiyeb2b
+package com.opakyazilim.yasirb2b
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -1,9 +1,9 @@
 class SiteSabit { 
-   static String? FirmaAdi = "EMIRKIRTASIYE";  
-  static String? Link = "emirkirtasiye.com.tr"; 
-  static String? Com = "com.opakyazilim.emirkirtasiyeb2b"; 
-  static String? Versiyon = "0.0.1"; 
+   static String? FirmaAdi = "YASIR";  
+  static String? Link = "yasir.com.tr"; 
+  static String? Com = "com.opakyazilim.yasirb2b"; 
+  static String? Versiyon = "2.0.0"; 
   static String? Platform = "IOS"; 
-  static String? ExServisId = "9CCB2B24-7147-4C07-973D-4C6D1422AED9"; 
-  static String oneSignalKey = "eb3a10ad-200a-43fd-95ec-9b3479503ad7"; 
+  static String? ExServisId = "e5d26671-9cb7-43b9-bf0e-3edc68ffe326"; 
+  static String oneSignalKey = "1d039731-0f83-4560-a79b-5d3f6625beb2"; 
 }
