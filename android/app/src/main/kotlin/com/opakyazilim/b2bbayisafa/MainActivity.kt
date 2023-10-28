6b52d2c00299b1c7ac9bb5cf55e2b89c3c6cd6bf
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2023-10-28 14:45:17 +0300

    Gönderen Kullanıcı :Fatih Uygulama : BAYISAFA

--- android/app/src/main/kotlin/com/opakyazilim/b2bbayisafa/MainActivity.kt
+++ android/app/src/main/kotlin/com/opakyazilim/b2bbayisafa/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.opakyazilim.merkezsenb2b
+package com.opakyazilim.b2bbayisafa
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -1,9 +1,9 @@
 class SiteSabit { 
-   static String? FirmaAdi = "MERKEZSEN";  
-  static String? Link = "www.merkezsen.com.tr"; 
-  static String? Com = "com.opakyazilim.merkezsenb2b"; 
+   static String? FirmaAdi = "BAYISAFA";  
+  static String? Link = "bayisafa.com"; 
+  static String? Com = "com.opakyazilim.b2bbayisafa"; 
   static String? Versiyon = "0.0.1"; 
   static String? Platform = "IOS"; 
-  static String? ExServisId = "5D178C9C-40D6-4E93-9C8B-1D31944FEB9A"; 
-  static String oneSignalKey = "db0e18bd-6ffb-4a7a-a216-9107e6fe261e"; 
+  static String? ExServisId = "734FD396-C57E-4CB5-A8AF-DA8639334580"; 
+  static String oneSignalKey = "933856c7-44af-4352-9103-92acefd5d184"; 
 }
