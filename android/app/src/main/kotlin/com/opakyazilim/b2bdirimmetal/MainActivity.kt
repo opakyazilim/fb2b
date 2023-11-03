4949f847e24f934164bef39b641a49a1ec1b7d29
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2023-11-03 18:14:35 +0200

    Gönderen Kullanıcı :Fatih Uygulama : DIRIMMETAL

--- android/app/src/main/kotlin/com/opakyazilim/b2bdirimmetal/MainActivity.kt
+++ android/app/src/main/kotlin/com/opakyazilim/b2bdirimmetal/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.opakyazilim.akkayadagitim
+package com.opakyazilim.b2bdirimmetal
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -1,9 +1,9 @@
 class SiteSabit { 
-   static String? FirmaAdi = "AKKAYADAGITIM";  
-  static String? Link = "akkayadagitim.com"; 
-  static String? Com = "com.opakyazilim.akkayadagitim"; 
+   static String? FirmaAdi = "DIRIMMETAL";  
+  static String? Link = "bayi.dirimmetal.com"; 
+  static String? Com = "com.opakyazilim.b2bdirimmetal"; 
   static String? Versiyon = "2.0.0"; 
   static String? Platform = "IOS"; 
-  static String? ExServisId = "da834d33-a6b0-4964-958c-26ad67a78f1d"; 
-  static String oneSignalKey = "2d09bea4-ddb3-4f8f-9465-72912a7f1ffb"; 
+  static String? ExServisId = "86af25f6-5c52-4420-a81c-df0c75de227c"; 
+  static String oneSignalKey = ""; 
 }
