2992d7ae1ffe1513d951ab3085480eb11a80c6af
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2023-12-15 15:12:30 +0200

    Gönderen Kullanıcı :Fatih Uygulama : DEFNEDEFTER

--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -2,7 +2,7 @@ class SiteSabit {
    static String? FirmaAdi = "DEFNEDEFTER";  
   static String? Link = "eplasiyer.defnedefter.com"; 
   static String? Com = "com.opakyazilim.b2bdefnedefter"; 
-  static String? Versiyon = "0.0.2"; 
+  static String? Versiyon = "0.0.3"; 
   static String? Platform = "IOS"; 
   static String? ExServisId = "58135ae8-158a-4445-bced-70c31ffe8847"; 
   static String oneSignalKey = "90912ce7-3d39-4ba5-8af8-1242fab14240"; 
