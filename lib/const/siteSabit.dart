9b55f75ca120c2a59492a28fdc8d6ccae43d0a37
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2023-12-16 16:00:38 +0200

    Gönderen Kullanıcı :Fatih Uygulama : ERAYSAN

--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -2,7 +2,7 @@ class SiteSabit {
    static String? FirmaAdi = "ERAYSAN";  
   static String? Link = "b4b.eraysan.com.tr"; 
   static String? Com = "com.opakyazilim.eraysanb4b"; 
-  static String? Versiyon = "4.0.1"; 
+  static String? Versiyon = "5.0.0"; 
   static String? Platform = "IOS"; 
   static String? ExServisId = "2dda55c3-d605-486d-9ada-da00949eec36"; 
   static String oneSignalKey = "cbc4605b-3f77-4b87-837e-ed69a06d7216"; 
