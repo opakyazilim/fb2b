ec64a7f8b3cfa37826b4f2a783fab7db391765aa
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-02-07 11:54:14 +0300

    Gönderen Kullanıcı :Fatih Uygulama : BATIKIRTASIYE

--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -2,7 +2,7 @@ class SiteSabit {
    static String? FirmaAdi = "BATIKIRTASIYE";  
   static String? Link = "b2b.batikirtasiye.com"; 
   static String? Com = "com.opakyazilim.b2bbatikirtasiye"; 
-  static String? Versiyon = "0.0.5"; 
+  static String? Versiyon = "0.0.6"; 
   static String? Platform = "IOS"; 
   static String? ExServisId = "af4e79f5-2c0f-438d-9a6f-30fac4a20e4f"; 
   static String oneSignalKey = "8e934daf-e52b-42b7-be80-ee554ab3b99a"; 
