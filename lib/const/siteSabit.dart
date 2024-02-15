4731915d3c43d0e1b1b3c957a14fc396a02fe624
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-02-15 17:14:17 +0300

    Gönderen Kullanıcı :Fatih Uygulama : BILGIKARE

--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -2,7 +2,7 @@ class SiteSabit {
    static String? FirmaAdi = "BILGIKARE";  
   static String? Link = "www.bilgikare.com"; 
   static String? Com = "com.opakyazilim.b4bbilgikare"; 
-  static String? Versiyon = "0.0.3"; 
+  static String? Versiyon = "0.0.1"; 
   static String? Platform = "IOS"; 
   static String? ExServisId = "22690737-aeab-4f64-a7cc-7b6710de21ab"; 
   static String oneSignalKey = "74ab2e46-7997-42ca-b4ce-157a99698459"; 
