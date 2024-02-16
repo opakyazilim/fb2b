a4e86e68a0e04760ff4b11fc29bfdd3f2c9fc582
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-02-16 15:02:15 +0300

    Gönderen Kullanıcı :Fatih Uygulama : ABAKLIHALI

--- android/app/src/main/kotlin/com/opakyazilim/b2babaklihali/MainActivity.kt
+++ android/app/src/main/kotlin/com/opakyazilim/b2babaklihali/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.opakyazilim.b2bsenses
+package com.opakyazilim.b2babaklihali
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -1,9 +1,9 @@
 class SiteSabit { 
-   static String? FirmaAdi = "SENSESKIRTASIYE";  
-  static String? Link = "sensesb2b.com"; 
-  static String? Com = "com.opakyazilim.b2bsenses"; 
+   static String? FirmaAdi = "ABAKLIHALI";  
+  static String? Link = "b2b.abaklihali.com"; 
+  static String? Com = "com.opakyazilim.b2babaklihali"; 
   static String? Versiyon = "0.0.1"; 
   static String? Platform = "IOS"; 
-  static String? ExServisId = "6C662166-C447-40F6-97AD-38E3F2B3F6B2"; 
-  static String oneSignalKey = "6295f9dc-30e3-4c93-b261-ed04384f74c4"; 
+  static String? ExServisId = "0ca414b1-369c-4a92-a588-ff2c9140d308"; 
+  static String oneSignalKey = "b0406a37-7942-4d18-a45d-ae0ab3d46b33"; 
 }
