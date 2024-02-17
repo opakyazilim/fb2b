9191a7d3bc88923aef2dcb100fc2688876d22b84
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-02-17 12:07:30 +0300

    Gönderen Kullanıcı :Fatih Uygulama : YUCEGROUP

--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -2,7 +2,7 @@ class SiteSabit {
    static String? FirmaAdi = "YUCEGROUP";  
   static String? Link = "b2b.xn--ycegroup-65a.com"; 
   static String? Com = "com.opakyazilim.yucegroupb2b"; 
-  static String? Versiyon = "0.0.2"; 
+  static String? Versiyon = "0.0.3"; 
   static String? Platform = "IOS"; 
   static String? ExServisId = "318d6baa-f5b5-4c1b-b051-5447573fd92d"; 
   static String oneSignalKey = "502874d1-511a-4daf-b286-ca5aec6a397a"; 
