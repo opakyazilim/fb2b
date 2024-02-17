69446ed35fb4da0ab9ac3d77872b13a14faed65d
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-02-17 16:59:32 +0300

    Gönderen Kullanıcı :Fatih Uygulama : AKCAYKIRTASIYE

--- android/app/src/main/kotlin/com/opakyazilim/akcaykirtasiyeb2b/MainActivity.kt
+++ android/app/src/main/kotlin/com/opakyazilim/akcaykirtasiyeb2b/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.opakyazilim.akgunb2b
+package com.opakyazilim.akcaykirtasiyeb2b
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -1,9 +1,9 @@
 class SiteSabit { 
-   static String? FirmaAdi = "AKGUNDAGİTİM";  
-  static String? Link = "b2b.akgundagitim.com"; 
-  static String? Com = "com.opakyazilim.akgunb2b"; 
-  static String? Versiyon = "0.1.0"; 
+   static String? FirmaAdi = "AKCAYKIRTASIYE";  
+  static String? Link = "b2b.akcaykirtasiye.com"; 
+  static String? Com = "com.opakyazilim.akcaykirtasiyeb2b"; 
+  static String? Versiyon = "3.0.2"; 
   static String? Platform = "IOS"; 
-  static String? ExServisId = "2a726534-a05a-4702-9bf2-976b491e2cc6"; 
-  static String oneSignalKey = "9a3e00c9-b6ae-4d80-a447-436190fe9169"; 
+  static String? ExServisId = "c07f4d71-ac9a-47f5-94c4-c9f0d353a7f7"; 
+  static String oneSignalKey = "d5a848b0-5dbf-4231-b758-9d9227b5054c"; 
 }
