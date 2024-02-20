f130a0792fbb2c4f851ae8131fc94962f1661075
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-02-20 16:19:16 +0300

    Gönderen Kullanıcı :Fatih Uygulama : KAMILTURK

--- android/app/src/main/kotlin/com/opakyazilim/kamilturkb2b/MainActivity.kt
+++ android/app/src/main/kotlin/com/opakyazilim/kamilturkb2b/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.opakyazilim.b2bhaktanyayin
+package com.opakyazilim.kamilturkb2b
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -1,9 +1,9 @@
 class SiteSabit { 
-   static String? FirmaAdi = "HAKTANYAYINLARI";  
-  static String? Link = "b2b.haktanyayin.com"; 
-  static String? Com = "com.opakyazilim.b2bhaktanyayin"; 
-  static String? Versiyon = "2.0.0"; 
+   static String? FirmaAdi = "KAMILTURK";  
+  static String? Link = "b2b.kamilturk.com"; 
+  static String? Com = "com.opakyazilim.kamilturkb2b"; 
+  static String? Versiyon = "0.0.6"; 
   static String? Platform = "IOS"; 
-  static String? ExServisId = "0ca414b1-369c-4a92-a588-ff2c9140d308"; 
-  static String oneSignalKey = "6052a50d-bffe-41e9-9a52-1f05a853540b"; 
+  static String? ExServisId = "18bd985c-4b25-46dd-9547-3b8b655abf92"; 
+  static String oneSignalKey = "06d41617-fcd7-42c0-be2f-47b6fc0b9c53"; 
 }
