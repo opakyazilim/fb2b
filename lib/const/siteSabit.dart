223e3e45c82639e21f66c46c45d82c0844127857
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-02-21 12:29:04 +0300

    Gönderen Kullanıcı :Fatih Uygulama : SENSESKIRTASIYE

--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -2,7 +2,7 @@ class SiteSabit {
    static String? FirmaAdi = "SENSESKIRTASIYE";  
   static String? Link = "sensesb2b.com"; 
   static String? Com = "com.opakyazilim.b2bsenses"; 
-  static String? Versiyon = "0.0.2"; 
+  static String? Versiyon = "0.0.3"; 
   static String? Platform = "IOS"; 
   static String? ExServisId = "6C662166-C447-40F6-97AD-38E3F2B3F6B2"; 
   static String oneSignalKey = "6295f9dc-30e3-4c93-b261-ed04384f74c4"; 
