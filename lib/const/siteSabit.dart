384889fc2cc1c03a37f4ad8784c18d60b940775a
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-03-03 17:16:23 -0800

    Gönderen Kullanıcı :Fatih Uygulama : GEDIKTARIM

--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -2,7 +2,7 @@ class SiteSabit {
    static String? FirmaAdi = "GEDIKTARIM";  
   static String? Link = "b2b.gediktarim.com"; 
   static String? Com = "com.opakyazilim.b2bgediktarim"; 
-  static String? Versiyon = "2.0.1"; 
+  static String? Versiyon = "2.0.2"; 
   static String? Platform = "IOS"; 
   static String? ExServisId = "1d760740-66ff-409d-a9ba-e8f38e0bc07a"; 
   static String oneSignalKey = "abb8c78f-5b65-42a6-b2ed-d07d9b8a91e6"; 
