31df50cc84eff2f4bfa75df3b0433dd41b4cd49e
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-03-07 14:13:50 -0800

    Gönderen Kullanıcı :Fatih Uygulama : BUYUKTUFAN

--- android/app/src/main/kotlin/com/opakyazilim/buyuktufanb2b/MainActivity.kt
+++ android/app/src/main/kotlin/com/opakyazilim/buyuktufanb2b/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.opakyazilim.timonkirtasiyeb2b
+package com.opakyazilim.buyuktufanb2b
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -1,10 +1,10 @@
 class SiteSabit { 
-   static String? FirmaAdi = "TIMONKIRTASIYE";  
-  static String? Link = "b2b.timon.com.tr"; 
-  static String? Com = "com.opakyazilim.timonkirtasiyeb2b"; 
-  static String? Versiyon = "1.0.0"; 
+   static String? FirmaAdi = "BUYUKTUFAN";  
+  static String? Link = "b4b.buyuktufan.com.tr"; 
+  static String? Com = "com.opakyazilim.buyuktufanb2b"; 
+  static String? Versiyon = "0.0.1"; 
   static String? Platform = "IOS"; 
-  static String? ExServisId = "aca0f229-c4b8-4d51-b872-3864b9849a5f"; 
-  static String oneSignalKey = "69be1b27-2889-41af-b72c-596537362252"; 
+  static String? ExServisId = "5bc45fca-192e-4798-b3f4-86bd06c444d2"; 
+  static String oneSignalKey = "e949aeca-1764-4eb9-a4d5-ee718a6d31ca"; 
   static String? Dil = "TR"; 
 }
