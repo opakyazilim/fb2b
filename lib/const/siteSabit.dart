12384e1103e5b2547731c1171049f787a14dcab6
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-03-19 17:21:12 -0700

    Gönderen Kullanıcı :Fatih Uygulama : Balonevi Export B2B

--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -2,7 +2,7 @@ class SiteSabit {
    static String? FirmaAdi = "Balonevi Export B2B";  
   static String? Link = "bayilik.balonevi.com"; 
   static String? Com = "com.opakyazilim.balonevien"; 
-  static String? Versiyon = "2.0.1"; 
+  static String? Versiyon = "2.0.2"; 
   static String? Platform = "IOS"; 
   static String? ExServisId = "af2314b2-15d0-44f0-b392-d57bf159a0d0"; 
   static String oneSignalKey = "5d3d751e-d098-4d30-be2a-62723f490de9"; 
