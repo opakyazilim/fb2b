295043389e13e4da42980ff38f051e6522f33467
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-03-22 10:32:04 -0700

    Gönderen Kullanıcı :Fatih Uygulama : Deneme Kulübü B2B

--- android/app/src/main/kotlin/com/opakyazilim/b2bdenemekulubu/MainActivity.kt
+++ android/app/src/main/kotlin/com/opakyazilim/b2bdenemekulubu/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.opakyazilim.blttekniktarim
+package com.opakyazilim.b2bdenemekulubu
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -1,10 +1,10 @@
 class SiteSabit { 
-   static String? FirmaAdi = "Blt Teknik Tarım B2B";  
-  static String? Link = "b2b.blttekniktarim.com"; 
-  static String? Com = "com.opakyazilim.blttekniktarim"; 
-  static String? Versiyon = "0.0.1"; 
+   static String? FirmaAdi = "Deneme Kulübü B2B";  
+  static String? Link = "denemekulubu.com"; 
+  static String? Com = "com.opakyazilim.b2bdenemekulubu"; 
+  static String? Versiyon = "2.0.2"; 
   static String? Platform = "IOS"; 
-  static String? ExServisId = "b6fe1107-8e67-43f9-89bd-01b94090cdf8"; 
-  static String oneSignalKey = "b6fe1107-8e67-43f9-89bd-01b94090cdf8"; 
+  static String? ExServisId = "5D178C9C-40D6-4E93-9C8B-1D31944FEB9A"; 
+  static String oneSignalKey = "5D178C9C-40D6-4E93-9C8B-1D31944FEB9A"; 
   static String? Dil = "TR"; 
 }
