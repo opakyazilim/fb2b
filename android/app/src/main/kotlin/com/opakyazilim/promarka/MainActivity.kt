1e9964533790b4c17211a01dd0756b52ffbfd4ce
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-03-22 12:36:24 -0700

    Gönderen Kullanıcı :Fatih Uygulama : Promarka

--- android/app/src/main/kotlin/com/opakyazilim/promarka/MainActivity.kt
+++ android/app/src/main/kotlin/com/opakyazilim/promarka/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.opakyazilim.kdb2b
+package com.opakyazilim.promarka
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -1,10 +1,10 @@
 class SiteSabit { 
-   static String? FirmaAdi = "KD Elektrik B2B";  
-  static String? Link = "b2b.kdelektrik.com.tr"; 
-  static String? Com = "com.opakyazilim.kdb2b"; 
-  static String? Versiyon = "0.0.4"; 
+   static String? FirmaAdi = "Promarka";  
+  static String? Link = "promarka.com.tr"; 
+  static String? Com = "com.opakyazilim.promarka"; 
+  static String? Versiyon = "0.0.3"; 
   static String? Platform = "IOS"; 
-  static String? ExServisId = "49579ba0-6d14-42c2-9de5-6104767cfc84"; 
-  static String oneSignalKey = "49579ba0-6d14-42c2-9de5-6104767cfc84"; 
+  static String? ExServisId = "a55a6658-5b37-4860-bafc-bdf8a4abb6f9"; 
+  static String oneSignalKey = "a55a6658-5b37-4860-bafc-bdf8a4abb6f9"; 
   static String? Dil = "TR"; 
 }
