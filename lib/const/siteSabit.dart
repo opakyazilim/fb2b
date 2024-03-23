06b67ba5c6a140ff9cd9c04e0e482be8d30b9da5
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-03-22 17:20:07 -0700

    Gönderen Kullanıcı :Fatih Uygulama : Provita Pharma

--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -2,7 +2,7 @@ class SiteSabit {
    static String? FirmaAdi = "Provita Pharma";  
   static String? Link = "b4b.provitapharma.com.tr"; 
   static String? Com = "com.opakyazilim.provitab2b"; 
-  static String? Versiyon = "0.0.2"; 
+  static String? Versiyon = "0.0.1"; 
   static String? Platform = "IOS"; 
   static String? ExServisId = "96073073-8080-4f73-a14e-47cdba8573fc"; 
   static String oneSignalKey = "96073073-8080-4f73-a14e-47cdba8573fc"; 
