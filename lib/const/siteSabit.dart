0e25d81998adfcca04a3cf4fa0170544650832f2
Fatih Akbayrak <fatih.akbayrak@opakyazilim.com>
2024-03-26 16:47:40 -0700

    Gönderen Kullanıcı :Fatih Uygulama : Yasir B2B

--- lib/const/siteSabit.dart
+++ lib/const/siteSabit.dart
@@ -2,9 +2,9 @@ class SiteSabit {
    static String? FirmaAdi = "Yasir B2B";  
   static String? Link = "yasir.com.tr"; 
   static String? Com = "com.opakyazilim.yasirb2b"; 
-  static String? Versiyon = "2.1.6"; 
+  static String? Versiyon = "2.1.7"; 
   static String? Platform = "IOS"; 
   static String? ExServisId = "e5d26671-9cb7-43b9-bf0e-3edc68ffe326"; 
-  static String oneSignalKey = "e5d26671-9cb7-43b9-bf0e-3edc68ffe326"; 
+  static String oneSignalKey = "2924c8f9-dc0b-4d48-96b4-a727c927fbf5"; 
   static String? Dil = "TR"; 
 }
